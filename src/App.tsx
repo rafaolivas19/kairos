9856af7cb94f7a24c4dabe96033726b98e03b80b
Rafa Olivas <rafaolivas19@gmail.com>
2026-01-17 11:55:12 -0700

    Installed dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { Button } from './components/ui/button'
+import { ThemeProvider } from './components/ui/theme-provider'
 
 function App() {
   return (
-    <>
-      Holis
-    </>
+    <div className='h-screen w-screen flex items-center justify-center'>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Button onClick={() => { alert('Holis') }}>Holis</Button>
+      </ThemeProvider>
+    </div>
   )
 }
 
